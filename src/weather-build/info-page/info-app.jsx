4e11f415dd66043601fe7b61b0440114cebb44e9
Michał Łoś <michallos1>
2022-06-18 19:34:07 +0200

    update

--- src/weather-build/info-page/info-app.jsx
+++ src/weather-build/info-page/info-app.jsx
@@ -3,14 +3,16 @@ import React from "react";
 
 const InfoApp = ({data}) => (
     <div className="weather-info">
-        <div className="city-name"> {data.name} </div>
-        <div className="temperature"> Temperature: {data.main.temp} &deg;C </div>
-        <div className="humidity"> Humidity: {data.main.humidity} %</div>
-        <div className="hpa"> Pressure: {data.main.pressure}</div>
-        <div className="sunrise"> Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-PL')}</div>
-        <div className="sunset"> Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-PL')}</div>
-        {/* <div>Teraz {date}</div> */}
-        <div></div>
+        <div className="op1">
+            <div className="city-name"> {data.name} </div>
+            <div className="temperature"> Temperature: {data.main.temp} &deg;C </div>
+            <div className="humidity"> Humidity: {data.main.humidity} %</div>
+            <div className="hpa"> Pressure: {data.main.pressure}  hPa</div>
+        </div>
+        <div className="op2">
+            <div className="sunrise"> Sunrise: {new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-PL')}</div>
+            <div className="sunset"> Sunset: {new Date(data.sys.sunset * 1000).toLocaleTimeString('en-PL')}</div>
+        </div>
     </div>
 )
 
